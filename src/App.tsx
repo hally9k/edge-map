b0aa94a691f5cae2a5f5bf793efcdc05eea4e656
Hal Smith Stevens <hally9k@gmail.com>
2020-05-05 08:06:25 +1200

    Add tracker name input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+
 import './App.css';
 
-function App() {
+interface LatLng {
+  lat: number,
+  lng: number
+}
+interface Props {
+  google: any
+}
+
+interface Device extends LatLng {
+  device: string
+}
+
+const CENTER: LatLng = {
+  lng: 174.76604461669922,
+  lat: -36.853526940982796
+}
+
+function App({ google }: Props) {
+  const [map, setMap] = useState<any>(null)
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const [trackerName, setTrackerName] = useState<string>('')
+  const [tracker, setTracker] = useState<Device | null>(null)
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (trackerName) {
+        try {
+          fetch(`https://lqltw7pnb3.execute-api.ap-southeast-2.amazonaws.com/alpha?tracker=${trackerName}`)
+            .then(response => response.json())
+            .then(({ data }) => {
+              setTracker((prevValue) => {
+                if (!prevValue || data.device !== prevValue.device) {
+                  if (map) {
+                    map.panTo(data)
+                  }
+
+                  return data
+                }
+
+                prevValue.lat = data.lat
+                prevValue.lng = data.lng
+
+                return prevValue
+              })
+            });
+        } catch (e) { }
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [map, trackerName])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <input className="tracker-input" onChange={({ currentTarget: { value } }) => setInputValue(value)} />
+        <button className="tracker-button" onClick={() => setTrackerName(inputValue)}>Track</button>
+      </div>
+      <Map onReady={(_, map) => setMap(map)} google={google} initialCenter={CENTER} zoom={13} disableDefaultUI={true}
+      >
+        {tracker &&
+          <Marker position={tracker} title={tracker.device} ></Marker>
+        }
+      </Map>
     </div>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBYw9p2ubyPqqK98y_g-uXWY-Dxvs8GXAY'
+})(App)

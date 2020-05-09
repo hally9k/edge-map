4409bba5e634f97baffb7d9f6027a45926c7a689
Hal Smith Stevens <hally9k@gmail.com>
2020-05-09 14:43:48 +1200

    Added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+import React from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { TrackerMap } from 'components/map'
+import { Auth } from 'components/auth'
+import { AuthContextProvider } from './state/auth-context'
 
-import './App.css';
+import './App.css'
 
-interface LatLng {
-  lat: number,
-  lng: number
-}
-interface Props {
-  google: any
-}
-
-interface Device extends LatLng {
-  device: string
-}
-
-const CENTER: LatLng = {
-  lng: 174.76604461669922,
-  lat: -36.853526940982796
-}
-
-function App({ google }: Props) {
-  const [map, setMap] = useState<any>(null)
-  const [inputValue, setInputValue] = useState<string>('')
-
-  const [trackerName, setTrackerName] = useState<string>('')
-  const [tracker, setTracker] = useState<Device | null>(null)
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (trackerName) {
-        try {
-          fetch(`https://lqltw7pnb3.execute-api.ap-southeast-2.amazonaws.com/alpha?tracker=${trackerName}`)
-            .then(response => response.json())
-            .then(({ data }) => {
-              setTracker((prevValue) => {
-                if (!prevValue || data.device !== prevValue.device) {
-                  if (map) {
-                    map.panTo(data)
-                  }
-
-                  return data
-                }
-
-                prevValue.lat = data.lat
-                prevValue.lng = data.lng
-
-                return prevValue
-              })
-            });
-        } catch (e) { }
-      }
-    }, 1000)
-
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [map, trackerName])
-
+function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <input className="tracker-input" onChange={({ currentTarget: { value } }) => setInputValue(value)} />
-        <button className="tracker-button" onClick={() => setTrackerName(inputValue)}>Track</button>
-      </div>
-      <Map onReady={(_, map) => setMap(map)} google={google} initialCenter={CENTER} zoom={13} disableDefaultUI={true}
-      >
-        {tracker &&
-          <Marker position={tracker} title={tracker.device} ></Marker>
-        }
-      </Map>
+      <BrowserRouter>
+        <Route path="/">
+          <AuthContextProvider>
+            <Auth>
+              <Switch>
+                <Route path="/" render={() => <TrackerMap />} />
+              </Switch>
+            </Auth>
+          </AuthContextProvider>
+        </Route>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBYw9p2ubyPqqK98y_g-uXWY-Dxvs8GXAY'
-})(App)
+export default App
